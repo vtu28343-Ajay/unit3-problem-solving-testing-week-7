dbd9551e1d5e545d8f8c47c5c8b71b35bfb044a9
vtu28343-Ajay <vtu28343@veltech.edu.in>
2026-09-03 12:28:52 +0530

    Time: 254 ms (33.61%), Space: 119.1 MB (62.56%) - LeetHub

---
+++ 1600-throne-inheritance/1600-throne-inheritance.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class ThroneInheritance {
+    private String king;
+    private Map<String, List<String>> children;
+    private Set<String> dead;
+
+    public ThroneInheritance(String kingName) {
+        king = kingName;
+        children = new HashMap<>();
+        dead = new HashSet<>();
+    }
+
+    public void birth(String parentName, String childName) {
+        children.putIfAbsent(parentName, new ArrayList<>());
+        children.get(parentName).add(childName);
+    }
+
+    public void death(String name) {
+        dead.add(name);
+    }
+
+    public List<String> getInheritanceOrder() {
+        List<String> order = new ArrayList<>();
+        dfs(king, order);
+        return order;
+    }
+
+    private void dfs(String person, List<String> order) {
+        if (!dead.contains(person)) {
+            order.add(person);
+        }
+
+        if (children.containsKey(person)) {
+            for (String child : children.get(person)) {
+                dfs(child, order);
+            }
+        }
+    }
+}
